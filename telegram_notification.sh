0b9fec5b555da87aba7af9f37fa8dd59f64181e8
Michelle <michelleamesquita@gmail.com>
2022-01-30 20:24:00 +0000

    Add new file

---
+++ telegram_notification.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Get the token from Travis environment vars and build the bot URL:
+BOT_URL="https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage"
+
+# Set formatting for the message. Can be either "Markdown" or "HTML"
+PARSE_MODE="Markdown"
+
+# Use built-in Travis variables to check if all previous steps passed:
+if [ $CI_JOB_STATUS == 'success']; then
+    build_status="succeeded"
+else
+    build_status="failed"
+fi
+
+# Define send message function. parse_mode can be changed to
+# HTML, depending on how you want to format your message:
+send_msg () {
+    curl -s -X POST ${BOT_URL} -d chat_id=$TELEGRAM_CHAT_ID \
+        -d text="$1" -d parse_mode=${PARSE_MODE}
+}
+
+# Send message to the bot with some pertinent details about the job
+# Note that for Markdown, you need to escape any backtick (inline-code)
+# characters, since they're reserved in bash
+send_msg "
+-------------------------------------
+Travis build * {$CI_JOB_STATUS}!*
+\`Repository:  ${CI_PROJECT_DIR}\`
+\`Branch:      ${CI_COMMIT_BRANCH}\`
+*Commit Msg:*
+${CI_COMMIT_MESSAGE	}
+)
+--------------------------------------
+"

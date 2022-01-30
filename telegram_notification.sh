a5e57d510c5c282157bf4b92bef6a8b8d4d3970f
Michelle <michelleamesquita@gmail.com>
2022-01-30 21:59:26 +0000

    Update telegram_notification.sh

--- telegram_notification.sh
+++ telegram_notification.sh
@@ -8,7 +8,7 @@ PARSE_MODE="Markdown"
 
 # echo "${CI_JOB_STATUS}"
 # Use built-in Travis variables to check if all previous steps passed:
-if [ ${CI_JOB_STATUS} == 'success' ]; then
+if [${CI_JOB_STATUS} == 'success']; then
     build_status="succeeded"
 else
     build_status="failed"
@@ -16,15 +16,9 @@ fi
 
 # Define send message function. parse_mode can be changed to
 # HTML, depending on how you want to format your message:
-send_msg () {
-    curl -s -X POST ${BOT_URL} -d chat_id=$TELEGRAM_CHAT_ID \
-        -d text="$1" -d parse_mode=${PARSE_MODE}
-}
 
-# Send message to the bot with some pertinent details about the job
-# Note that for Markdown, you need to escape any backtick (inline-code)
-# characters, since they're reserved in bash
-send_msg "
+curl -s -X POST ${BOT_URL} -d chat_id=$TELEGRAM_CHAT_ID \
+        -d text="
 -------------------------------------
 Gitlab build * ${CI_JOB_STATUS}!*
 \`Repository:  ${CI_PROJECT_DIR}\`
@@ -33,4 +27,6 @@ Gitlab build * ${CI_JOB_STATUS}!*
 ${CI_COMMIT_MESSAGE	}
 )
 --------------------------------------
-"
+" -d parse_mode=${PARSE_MODE}
+
+

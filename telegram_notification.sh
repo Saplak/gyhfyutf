b0a88207d96ec72238e56737c56d8100e092f446
Michelle <michelleamesquita@gmail.com>
2022-01-30 22:25:14 +0000

    Update telegram_notification.sh

--- telegram_notification.sh
+++ telegram_notification.sh
@@ -25,7 +25,6 @@ ${CI_COMMIT_MESSAGE}
 --------------------------------------
 "
 
-curl -s -X POST ${BOT_URL} -d chat_id=$TELEGRAM_CHAT_ID \
-        -d text=${MESSAGE} -d parse_mode=${PARSE_MODE}
+curl -s -X POST ${BOT_URL} -d chat_id=$TELEGRAM_CHAT_ID -d text="${MESSAGE}" -d parse_mode=${PARSE_MODE}
 
 
